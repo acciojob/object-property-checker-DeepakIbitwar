5af2b29b7b7158f83a56664359be65b0e227ef59
DeepakIbitwar <127574226+DeepakIbitwar@users.noreply.github.com>
2023-04-25 13:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
   For(let i in sampleObject){
-    if(i===key){
+    if(i===key)
         return true;
-    }
-    return false;
+    
+
   }
-	
+	    return false;
 }
 
 // Do not change the code below
